c7c70ec7bfc19fb22e0e8ff47a94ba673b9bd2b6
Yuri <chocolat@w7.dion.ne.jp>
2020-01-01 09:10:19 +0900

    Add emotion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
-import logo from '../img/logo.svg';
+/** @jsx jsx */
+import { Global, css, jsx } from '@emotion/core';
 
-import '../styles/style.scss';
+import logo from '../img/logo.svg';
+import general from '../styles/style.scss';
 
 const App = () => {
   return (
-    <div className="app">
-      <header className="app_header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <main>
-        <p>Hello React Application</p>
-      </main>
-    </div>
+    <React.Fragment>
+      <Global styles={css`${general}`} />
+      <div css={style}>
+        <header css={header}>
+          <img src={logo} alt="React" />
+        </header>
+        <main>
+          <p>Hello React Application</p>
+        </main>
+      </div>
+    </React.Fragment>
   );
-}
+};
+
+const style = css`
+  padding: 2.4rem 5%;
+
+  @media screen and (min-width: 900px) {
+    padding: 2.4rem calc((100% - 720px) / 2);
+  }
+`;
+
+const header = css`
+  margin: 0 0 2.4rem;
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 700;
+    letter-spacing: .05em;
+  }
+`;
 
 export default App;
