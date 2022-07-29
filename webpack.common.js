170ed3b26dd6b3949c11043f29c76c8a529bb440
yurie.f <chocolat@w7.dion.ne.jp>
2022-07-29 20:41:54 +0900

    Change: typescript + webpack5

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,27 @@
-const path = require('path');
+const path = require('path')
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const AutoPrefixer = require('autoprefixer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
-const IS_DEV = (process.env.NODE_ENV === 'dev');
+const IS_DEV = process.env.NODE_ENV === 'dev'
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
-    app: './index.js',
+    app: './index.tsx',
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './index.html',
-      filename: './index.html'
-    })
+    }),
+    new ESLintPlugin(),
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'img/[hash][ext][query]',
   },
   module: {
     rules: [
@@ -30,12 +31,14 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react"
-            ]
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.ts[x]?$/,
+        exclude: /(node_modules)/,
+        loader: 'ts-loader',
       },
       {
         test: /\.(css|scss)$/,
@@ -45,50 +48,47 @@ module.exports = {
             loader: 'css-loader',
             options: {
               url: false,
-              // modules: true,
-              sourceMap: true,
-            }
+              modules: true,
+              sourceMap: IS_DEV,
+              importLoaders: 2,
+            },
           },
           {
             loader: 'postcss-loader',
             options: {
-              plugins: [
-                AutoPrefixer({
-                  overrideBrowserslist: ['last 4 versions'],
-                  grid: 'autoplace'
-                })
-              ]
-            }
+              postcssOptions: {
+                plugins: [
+                  [
+                    'autoprefixer',
+                    {
+                      grid: true,
+                    },
+                  ],
+                ],
+              },
+            },
           },
           {
             loader: 'sass-loader',
             options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|ico)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'img/[name].[ext]'
-            }
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
+              sourceMap: IS_DEV,
             },
           },
         ],
-      }
-    ]
+      },
+      {
+        test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg|ico)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 100 * 1024,
+          },
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
-  }
-};
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css', '.scss'],
+  },
+  target: ['web'],
+}
